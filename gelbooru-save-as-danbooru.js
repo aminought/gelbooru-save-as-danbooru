c7fc55526a85d0ae5772c3b98d9ae85b1534c087
Alexander Logumanov <aminought@gmail.com>
2018-09-22 19:06:22 +0500

    Add downloading with predefined name by clicking on image

--- gelbooru-save-as-danbooru.js
+++ gelbooru-save-as-danbooru.js
@@ -7,34 +7,34 @@
 // ==/UserScript==
 
 function getNamesByTag(tag) {
-	elements = document.getElementsByClassName(tag);
-	names = []
-	for (var i=0; i < elements.length; i++) {
-		name = elements[i].children[1].text;
-		names.push(name);
-	}
-	return names;
+    elements = document.getElementsByClassName(tag);
+    names = []
+    for (var i=0; i < elements.length; i++) {
+        name = elements[i].children[1].text;
+        names.push(name);
+    }
+    return names;
 }
 
 function getImage() {
-	image = document.getElementById('image');
-	return image;
+    image = document.getElementById('image');
+    return image;
 }
 
 function getImageUrl() {
-	url = getImage().src;
-	return url;
+    url = getImage().src;
+    return url;
 }
 
 function getImageId(url) {
-	ch = url.search('_') != -1 ? '_' : '/';
-	id = url.split(ch).reverse()[0].split('.')[0];
-	return id;
+    ch = url.search('_') != -1 ? '_' : '/';
+    id = url.split(ch).reverse()[0].split('.')[0];
+    return id;
 }
 
 function getImageExt(url) {
-	ext = url.split('.').pop();
-	return ext;
+    ext = url.split('.').pop();
+    return ext;
 }
 
 function insertAfter(newNode, referenceNode) {
@@ -42,34 +42,35 @@ function insertAfter(newNode, referenceNode) {
 }
 
 function getDanbooruStyleName(char_names, artist_names, copyright_names, image_id, image_ext) {
-	char_str = char_names.join('_and_')
-	copyright_str = copyright_names.join('_and_')
-	artist_str = artist_names.join('_and_')
+    char_str = char_names.join('_and_')
+    copyright_str = copyright_names.join('_and_')
+    artist_str = artist_names.join('_and_')
 
-	image_name = `__${char_str}_${copyright_str}_drawn_by_${artist_str}__${image_id}.${image_ext}`
+    image_name = `__${char_str}_${copyright_str}_drawn_by_${artist_str}__${image_id}.${image_ext}`
 
-	image_name = image_name.replace(/[,\/#!$%\^&\*;:{}=\-~()]/g, '');
-	image_name = image_name.replace(/\s/g, '_');
-	return image_name;
+    image_name = image_name.replace(/[,\/#!$%\^&\*;:{}=\-~()]/g, '');
+    image_name = image_name.replace(/\s/g, '_');
+    return image_name;
+}
+
+function addDownloadLink(image_url, image_name) {
+    var link = document.createElement('a');
+    link.setAttribute('href', image_url);
+    link.setAttribute('download', image_name);
+    image_element = getImage();
+    image_element.parentNode.replaceChild(link, image_element);
+    link.appendChild(image_element);
 }
 
 window.onload = function() {
-	char_names = getNamesByTag('tag-type-character');
-	artist_names = getNamesByTag('tag-type-artist');
-	copyright_names = getNamesByTag('tag-type-copyright');
+    char_names = getNamesByTag('tag-type-character');
+    artist_names = getNamesByTag('tag-type-artist');
+    copyright_names = getNamesByTag('tag-type-copyright');
 
-	image_url = getImageUrl()
-	image_id = getImageId(url);
-	image_ext = getImageExt(url);
+    image_url = getImageUrl();
+    image_id = getImageId(url);
+    image_ext = getImageExt(url);
 
-	image_name = getDanbooruStyleName(char_names, artist_names, copyright_names, image_id, image_ext)
-
-	var br = document.createElement('br');
-	var link = document.createElement('a');
-	link.setAttribute('href', image_url);
-	link.setAttribute('download', image_name);
-	link.text = 'Click to save';
-	image_element = getImage();
-	insertAfter(br, image_element);
-	insertAfter(link, br);
+    image_name = getDanbooruStyleName(char_names, artist_names, copyright_names, image_id, image_ext);
+    addDownloadLink(image_url, image_name);
 }
