683d9e12ec01798cfec03fe64f689ba05bac2968
Alexander Logumanov <aminought@gmail.com>
2018-09-28 03:30:18 +0500

    Add workaround for #2

--- gelbooru-save-as-danbooru.js
+++ gelbooru-save-as-danbooru.js
@@ -71,6 +71,38 @@ function addDownloadLink(image_url, image_name) {
     link.appendChild(image_element);
 }
 
+// https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');  // Create a <textarea> element
+    el.value = str;                                 // Set its value to the string that you want copied
+    el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+    document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+    const selected =
+        document.getSelection().rangeCount > 0      // Check if there is any content selected previously
+            ? document.getSelection().getRangeAt(0) // Store selection if found
+            : false;                                // Mark as false to know no selection existed before
+    el.select();                                    // Select the <textarea> content
+    document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+    document.body.removeChild(el);                  // Remove the <textarea> element
+    if (selected) {                                 // If a selection existed before copying
+        document.getSelection().removeAllRanges();  // Unselect everything on the HTML document
+        document.getSelection().addRange(selected); // Restore the original selection
+    }
+};
+
+function addCopyNameButton(image_name) {
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click to copy name';
+    btn.addEventListener('click', function() {
+        copyToClipboard(image_name);
+    });
+    div = document.getElementById('right-col')
+    image = getImage();
+    div.insertBefore(btn, image.parentNode);
+}
+
 window.onload = function() {
     char_names = getNamesByTag('tag-type-character');
     artist_names = getNamesByTag('tag-type-artist');
@@ -81,5 +113,6 @@ window.onload = function() {
     image_ext = getImageExt(url);
 
     image_name = getDanbooruStyleName(char_names, artist_names, copyright_names, image_id, image_ext);
+    addCopyNameButton(image_name);
     addDownloadLink(image_url, image_name);
 }
